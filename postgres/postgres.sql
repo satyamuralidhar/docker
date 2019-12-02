675dd3d56c60acc66e80906a5f5b659884046503
satya <38804803+satyamuralidhar@users.noreply.github.com>
2019-12-02 11:48:21 +0530

    Update postgres.sql

--- postgres/postgres.sql
+++ postgres/postgres.sql
@@ -1,1 +1,56 @@
 
+INSERT INTO employees (first_name, last_name, department, email) 
+
+
+
+VALUES ('Lorenz', 'Vanthillo', 'IT', 'lvthillo@mail.com');
+
+
+
+
+
+
+
+INSERT INTO employees (first_name, last_name, department, email) 
+
+
+
+VALUES ('sai', 'behara', 'IT', 'sai@mail.com');
+
+
+
+
+
+
+
+INSERT INTO employees (first_name, last_name, department, email) 
+
+
+
+VALUES ('satya', 'peddireddi', 'IT', 'satya@mail.com');
+
+
+
+
+
+
+
+INSERT INTO employees (first_name, last_name, department, email) 
+
+
+
+VALUES ('prakriti', 'arora', 'IT', 'prakriti@mail.com');
+
+
+
+
+
+
+
+
+
+INSERT INTO employees (first_name, last_name, department, email) 
+
+
+
+VALUES ('nitish', 'sharma', 'IT', 'nitish@mail.com');
